10d30c6f8fedac0153d7c9aa20909dc5dbb44afd
Damian Nowak <108193148+Damian136@users.noreply.github.com>
2023-05-31 13:14:41 +0200

    xxx

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,20 +1,20 @@
-import Player from "@vimeo/player";
-import throttle from "lodash.throttle";
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
-const iframe = document.querySelector("#vimeo-player");
+const iframe = document.querySelector('#vimeo-player');
 const player = new Player(iframe);
 
 const saveCurrentTime = () => {
   player.getCurrentTime().then((seconds) => {
-    localStorage.setItem("videoplayer-current-time", seconds);
+    localStorage.setItem('videoplayer-current-time', seconds);
   });
 };
 
 const throttledSaveCurrentTime = throttle(saveCurrentTime, 1000);
 
-player.on("timeupdate", throttledSaveCurrentTime);
+player.on('timeupdate', throttledSaveCurrentTime);
 
-const currentTime = localStorage.getItem("videoplayer-current-time");
+const currentTime = localStorage.getItem('videoplayer-current-time');
 
 if (currentTime) {
   player.setCurrentTime(currentTime);
