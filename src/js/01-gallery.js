578c9a818bd54e0bca44f6d584b6da38c16e3eac
Damian Nowak <108193148+Damian136@users.noreply.github.com>
2023-05-31 13:14:07 +0200

    xxxx

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,20 +1,21 @@
 "use strict";
-import { galleryItems } from "./gallery-items.js";
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
 // Opisany w dokumentacji
 import SimpleLightbox from "simplelightbox";
 // Dodatkowy import stylów
 import "simplelightbox/dist/simple-lightbox.min.css";
 
-const gallery = document.querySelector(".gallery");
+
+const gallery = document.querySelector('.gallery');
 
 function createGalleryItem(item) {
-  const galleryItem = document.createElement("li");
-  const galleryLink = document.createElement("a");
-  galleryLink.classList.add("gallery__item");
-  const galleryImage = document.createElement("img");
-  galleryImage.classList.add("gallery__image");
-
+    const galleryItem = document.createElement('li');
+    const galleryLink = document.createElement('a');
+    galleryLink.classList.add('gallery__item')
+    const galleryImage = document.createElement('img');
+    galleryImage.classList.add('gallery__image')
+   
   galleryLink.href = item.original;
   galleryImage.src = item.preview;
   galleryImage.alt = item.description;
@@ -22,17 +23,15 @@ function createGalleryItem(item) {
   galleryLink.appendChild(galleryImage);
   galleryItem.appendChild(galleryLink);
 
-  return galleryItem;
-}
-
-const galleryItemsElements = galleryItems.map((item) =>
-  createGalleryItem(item)
-);
+     return galleryItem; 
+  }
+    
+const galleryItemsElements = galleryItems.map(item => createGalleryItem(item));
 gallery.append(...galleryItemsElements);
 
-const lightbox = new SimpleLightbox(".gallery a", {
-  captionsData: "alt",
-  captionDelay: "250",
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: '250',
 });
 
 console.log(galleryItems);
