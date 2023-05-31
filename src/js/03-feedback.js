3b57f3e1dbf7de88dbe7b221c830cde7ec59ff0c
Damian Nowak <108193148+Damian136@users.noreply.github.com>
2023-05-31 13:15:29 +0200

    xxx

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,38 +1,33 @@
-'use strict';
-
 import throttle from 'lodash.throttle';
-
 const form = document.querySelector('.feedback-form');
-const email = document.querySelector('[name="email"]');
-const message = document.querySelector('[name="message"]');
+const emailInput = form.querySelector('[name="email"]');
+const messageInput = form.querySelector('[name="message"]');
 
 const saveFeedbackState = () => {
   const feedbackState = {
-    email: email.value,
-    message: message.value,
+    email: emailInput.value,
+    message: messageInput.value,
   };
   localStorage.setItem('feedback-form-state', JSON.stringify(feedbackState));
 };
 
-email.addEventListener('input', throttle(saveFeedbackState, 500));
-message.addEventListener('input', throttle(saveFeedbackState, 500));
+emailInput.addEventListener('input', throttle(saveFeedbackState, 500));
+messageInput.addEventListener('input', throttle(saveFeedbackState, 500));
 
-const storedFeedbackState = JSON.parse(
-  localStorage.getItem('feedback-form-state')
-);
+const savedFeedbackState = JSON.parse(localStorage.getItem('feedback-form-state'));
 
-if (storedFeedbackState) {
-  email.value = storedFeedbackState.email;
-  message.value = storedFeedbackState.message;
+if (savedFeedbackState) {
+  emailInput.value = savedFeedbackState.email;
+  messageInput.value = savedFeedbackState.message;
 }
 
-form.addEventListener('submit', event => {
+form.addEventListener('submit', (event) => {
   event.preventDefault();
-
+  
   localStorage.removeItem('feedback-form-state');
 
-  email.value = '';
-  message.value = '';
+  emailInput.value = '';
+  messageInput.value = '';
 
-  console.log('Feedback state:', storedFeedbackState);
+  console.log('Feedback state:', savedFeedbackState);
 });
